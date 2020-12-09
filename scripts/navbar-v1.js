341e5507e71162796bb187e0c05a8cfb3fd69c13
jchans <jiangchenghans@gmail.com>
2020-12-09 16:29:25 +0800

    update navbar

--- scripts/navbar-v1.js
+++ scripts/navbar-v1.js
@@ -2,7 +2,7 @@ const template = document.createElement('template');
 template.innerHTML = `
 <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
         </div>
-        <a class="navbar-brand" href="/">老翰</a>
+        <a class="navbar-brand" href="/">老翰的儲藏室</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
