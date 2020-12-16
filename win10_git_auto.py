4a077e2fcd44dca808d026856e73febd7565f1fc
jchans <jiangchenghans@gmail.com>
2020-12-17 01:43:41 +0800

    add notes

--- win10_git_auto.py
+++ win10_git_auto.py
@@ -7,4 +7,7 @@ commitCommand = 'git commit -m ' + '"' + commitMessage + '"'
 os.system(commitCommand)
 os.system('git push')
 
-# test mac python2 compatibility
+# macbook 目前有些版本可能預設還是 python 2 ，有裝 python3
+# 可以用以下指令安裝，執行 venv
+# python3 -m venv tutorial-env
+# source tutorial-venv/bin/activate
