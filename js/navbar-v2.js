4d8ca29aeb3097a71499571344e55d4215c7dad6
jchans <jiangchenghans@gmail.com>
2020-12-24 10:49:59 +0800

    Change title

--- js/navbar-v2.js
+++ js/navbar-v2.js
@@ -3,7 +3,7 @@ template.innerHTML = `
 
     <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light navbar-custom">
         <div class="container">
-            <a class="navbar-brand" href="/">老翰的儲藏室</a>
+            <a class="navbar-brand" href="/">Hans' Dungeon</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
