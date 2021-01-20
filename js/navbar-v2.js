e6c76b1fd76c9f81a583b6ebbe6c0bb178c8c4c0
jchans <jiangchenghans@gmail.com>
2021-01-21 00:18:18 +0800

    organize navbar

--- js/navbar-v2.js
+++ js/navbar-v2.js
@@ -18,22 +18,17 @@ template.innerHTML = `
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="/">首頁</a>
+              <a class="nav-link" href="/cheatsheet.html">CheatSheets</a>
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="/about.html">關於老翰</a>
-            </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="https://github.com/jchans">Repo</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav">
             <li class="nav-item active">
               <a class="nav-link" href="/old-entries.html">舊記事</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="/cheatsheet.html">CheatSheets</a>
+              <a class="nav-link" href="/about.html">關於老翰</a>
             </li>
+            
+          </ul>
+          <ul class="navbar-nav">
           </ul>
         </div>
       </div>
